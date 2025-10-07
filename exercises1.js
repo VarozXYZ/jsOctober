404cee742d60d84c36596b7d79b67933c622dc49
VarozXYZ <alvaropiro@gmail.com>
2025-10-07 22:38:04 +0200

    exercise 17

--- exercises1.js
+++ exercises1.js
@@ -229,7 +229,7 @@ empleados.forEach(e => {
 // Debería imprimir:
 // Luis gana 1200 euros
 // Eva gana 1500 euros
-*/
+
 console.log("\nEjercicio 16: map básico");
 // Utiliza map para crear un nuevo array con el cubo de cada número del array 'numeros'
 const numeros = [1, 2, 3, 4, 5];
@@ -237,9 +237,11 @@ const cubos = numeros.map(n => n**3)
 // Datos de prueba:
 console.log(cubos);
 // Debería mostrar: [1, 8, 27, 64, 125]
-
+*/
 console.log("\nEjercicio 17: map con strings");
 // Utiliza map para crear un nuevo array con las palabras en mayúsculas
+const palabras = ["hola", "mundo", "js"];
+const mayus = palabras.map(p => p.toUpperCase())
 // Datos de prueba:
 console.log(mayus);
 // Debería mostrar: ['HOLA', 'MUNDO', 'JS']
